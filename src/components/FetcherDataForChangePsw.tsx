e909945c1d054809c27c6f4620e83ae7cafd7cf9
lynxdev <rojasricor@gmail.com>
2024-02-12 17:08:31 -0500

    Optimizing & Fixing Bugs

--- src/components/FetcherDataForChangePsw.tsx
+++ src/components/FetcherDataForChangePsw.tsx
@@ -20,8 +20,9 @@ function FetcherDataForChangePsw(): React.ReactNode {
     ({ temp }) => temp.tempDataForChangePsw
   );
 
-  const { data, error } = useQuery<any, any>(["userData", params.role], () =>
-    userService.getData(params.role)
+  const { data, error, isLoading } = useQuery<any, any>(
+    ["userData", params.role],
+    () => userService.getData(params.role)
   );
 
   useEffect(() => {
@@ -34,7 +35,11 @@ function FetcherDataForChangePsw(): React.ReactNode {
   return (
     <Paper sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }} elevation={6}>
       <Typography component="h1" variant="h5" gutterBottom align="center">
-        {tempDataStateForChangePsw.email || <CircularProgress size={20} />}
+        {tempDataStateForChangePsw.email && !isLoading ? (
+          tempDataStateForChangePsw.email
+        ) : (
+          <CircularProgress size={20} />
+        )}
       </Typography>
 
       <FormChangePsw userId={tempDataStateForChangePsw.id} />
