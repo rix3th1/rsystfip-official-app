27f9d113ad271656f13a9c1d0708eb1606ebb5b6
rojasricor <rojasricor@gmail.com>
2024-02-15 19:38:10 -0500

    Fixing type definition bug

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,12 +1,14 @@
 import NextAuth from "next-auth";
 
 declare module "next-auth" {
-  interface User {
-    id: string;
-    first_name: string;
-    last_name: string;
-    email: string;
-    role_name: string;
-    permissions: string[];
+  interface Session {
+    user: {
+      id: string;
+      first_name: string;
+      last_name: string;
+      email: string;
+      role_name: string;
+      permissions: string[];
+    };
   }
 }
